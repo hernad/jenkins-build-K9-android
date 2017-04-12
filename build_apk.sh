e6467d33d1fab18c565576e78a081c2e0777ac87
hernad <hernad@bring.out.ba>
2017-04-12 07:57:00 +0200

    reusable for multiple github users

--- build_apk.sh
+++ build_apk.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 
-GITHUB_USER=hernad
+GITHUB_USER=${1:-hernad}
 ANDROID_PROJECT=K9-android
 APK_DIR=/build/$ANDROID_PROJECT/k9mail/build/outputs/apk
 
 
-cd /build
+mkdir /build/$GITHUB_USER
+cd /build/$GITHUB_USER
+
 git clone https://github.com/$GITHUB_USER/$ANDROID_PROJECT.git
 
 
@@ -17,6 +19,7 @@ git log -1
 
 tools/build_only_bosanski.sh DEBUG
 cp -av $APK_DIR/* /apk/
+
 tools/build_only_bosanski.sh
 cp -av $APK_DIR/* /apk/
 
--- jenkins_build.sh
+++ jenkins_build.sh
@@ -11,7 +11,7 @@ ANDROID_PROJECT=K9-android
 
 docker rm -f android-build-$ANDROID_PROJECT
 
-echo === starting docker build $ANDROID_PROJECT in container android-build-$ANDROID_PROJECT ========
+echo === starting docker build $ANDROID_PROJECT in container android-build-$ANDROID_PROJECT / github: $GITHUB_USER  ========
 
 docker run -t \
        	-v $(pwd)/dot.android:/root/.android \
@@ -19,7 +19,7 @@ docker run -t \
        	-v $(pwd)/build:/build \
        	-v $(pwd)/apk:/apk \
 	-v $(pwd)/build_apk.sh:/build_apk.sh \
-       	--name android-build-$ANDROID_PROJECT android-dev /build_apk.sh
+       	--name android-build-$ANDROID_PROJECT android-dev /build_apk.sh $GITHUB_USER
 
 
 
