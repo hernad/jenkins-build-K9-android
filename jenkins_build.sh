32285f3b12e1cbc38ef4e4496292c4f0ecb4da39
hernad <hernad@bring.out.ba>
2017-04-12 08:26:24 +0200

    container name fix

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -8,10 +8,11 @@ fi
 GITHUB_USER=${1:-hernad}
 
 ANDROID_PROJECT=K9-android
+CONTAINER_NAME=android-build-$GITHUB_USER-$ANDROID_PROJECT
 
-docker rm -f android-build-$ANDROID_PROJECT
+docker rm -f $CONTAINER_NAME
 
-echo === starting docker build $ANDROID_PROJECT in container android-build-$ANDROID_PROJECT / github: $GITHUB_USER  ========
+echo === starting docker build $ANDROID_PROJECT in container $CONTAINER_NAME ========
 
 docker run -t \
        	-v $(pwd)/dot.android:/root/.android \
@@ -19,7 +20,7 @@ docker run -t \
        	-v $(pwd)/build:/build \
        	-v $(pwd)/apk:/apk \
 	-v $(pwd)/build_apk.sh:/build_apk.sh \
-       	--name android-build-$ANDROID_PROJECT android-dev /build_apk.sh $GITHUB_USER
+       	--name $CONTAINER_NAME /build_apk.sh $GITHUB_USER
 
 
 
