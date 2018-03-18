54ae6359bff7e5437212892da31d99987afc65d6
hernad <hernad@bring.out.ba>
2018-03-18 21:26:53 +0100

    k9mail-signed.apk

--- build_apk.sh
+++ build_apk.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+set -x
 
 GITHUB_USER=${1:-hernad}
 ANDROID_PROJECT=K9-android
-APK_DIR=/build/$GITHUB_USER/$ANDROID_PROJECT/k9mail/build/outputs/apk
+BUILD_ROOT=/build/$GITHUB_USER/$ANDROID_PROJECT
+
+APK_DIR=$BUILD_ROOT/k9mail/build/outputs/apk
 
 
 mkdir /build/$GITHUB_USER
@@ -12,19 +14,19 @@ cd /build/$GITHUB_USER
 git clone https://github.com/$GITHUB_USER/$ANDROID_PROJECT.git
 
 
-cd $ANDROID_PROJECT
+cd $BUILD_ROOT
 git checkout android -f
 git pull
 git log -1
 
 tools/build_only_bosanski.sh DEBUG
 APK_DIR_REL=k9mail/build/outputs/apk
-
 find $APK_DIR_REL
-
 cp -av $APK_DIR_REL/* /apk/
 
+cd $BUILD_ROOT
 tools/build_only_bosanski.sh
-tools/build_sign_apk.sh
+tools/bringout_sign_apk.sh
+find $APK_DIR_REL
 cp -av $APK_DIR_REL/* /apk/
 
